92b0fd6b83aa9c0ad93460e66531ca37de5ad2ec
baruh1988 <85126612+baruh1988@users.noreply.github.com>
2023-01-08 17:48:11 +0200

    fixed image import

--- client/cellshop/src/components/login.js
+++ client/cellshop/src/components/login.js
@@ -13,7 +13,7 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import bgImg from '../assets/phones.img';
+import bgImg from '../assets/phones.jpeg';
 
 const theme = createTheme();
 
