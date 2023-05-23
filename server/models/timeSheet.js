d911fee3352fbc0e8946f05efe95409545711857
avidery <41361786+avidery@users.noreply.github.com>
2023-05-23 09:24:38 +0300

    Update timeSheet.js
    Added auto generated id field
    Removed date field
    Added isOpen boolean field

--- server/models/timeSheet.js
+++ server/models/timeSheet.js
@@ -3,10 +3,16 @@ const Sequelize = require("sequelize");
 const connection = require("../middleware/connection");
 
 const TimeSheet = connection.define("timeSheet", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
   userId: Sequelize.INTEGER,
-  date: Sequelize.DATE,
   start: Sequelize.DATE,
   end: Sequelize.DATE,
+  isOpen: Sequelize.BOOLEAN
 });
 
 module.exports = TimeSheet;
