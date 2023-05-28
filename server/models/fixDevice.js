52683a1e74f8d46f80b1b569ea7fcdfe26424c50
avidery <41361786+avidery@users.noreply.github.com>
2023-05-28 22:28:41 +0300

    Update fixDevice.js
    Added auto generated id field

--- server/models/fixDevice.js
+++ server/models/fixDevice.js
@@ -2,6 +2,12 @@ const Sequelize = require("sequelize");
 const connection = require("../middleware/connection");
 
 const FixDevice = connection.define("fixDevice", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
     imei: Sequelize.INTEGER,
     inventoryId: Sequelize.INTEGER,
     inStock: Sequelize.BOOLEAN
