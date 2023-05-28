967d084678d668311872c0ddfd42e96f7ba7fdc3
avidery <41361786+avidery@users.noreply.github.com>
2023-05-28 22:28:36 +0300

    Update newDevice.js
    Added auto generated id field

--- server/models/newDevice.js
+++ server/models/newDevice.js
@@ -2,6 +2,12 @@ const Sequelize = require("sequelize");
 const connection = require("../middleware/connection");
 
 const NewDevice = connection.define("newDevice", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
     imei: Sequelize.INTEGER,
     inventoryId: Sequelize.INTEGER,
     inStock: Sequelize.BOOLEAN
